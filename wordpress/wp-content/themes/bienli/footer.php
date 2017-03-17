02bef8f91c60c03fc58da66d10d7c4b16a2e6253
Andy Ettisberger <andy@ettisberger.ch>
2017-03-17 23:26:38 +0100

    New styles for cake component

--- wordpress/wp-content/themes/bienli/footer.php
+++ wordpress/wp-content/themes/bienli/footer.php
@@ -2,8 +2,8 @@
 </main>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/vendor.js"></script>
 <!-- isotope doesnt work with bower concat-->
-<script src="<?php bloginfo('template_directory'); ?>/assets/vendor/isotope.pkgd.min.js"></script>
-<script src="<?php bloginfo('template_directory'); ?>/assets/vendor/packery-mode.pkgd.min.js"></script>
+<!--<script src="--><?php //bloginfo('template_directory'); ?><!--/assets/vendor/isotope.pkgd.min.js"></script>-->
+<!--<script src="--><?php //bloginfo('template_directory'); ?><!--/assets/vendor/packery-mode.pkgd.min.js"></script>-->
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/bienli.js"></script>
 <?php wp_footer(); ?>
 </body>
--- wordpress/wp-content/themes/bienli/includes/partials/cakes.partial.php
+++ wordpress/wp-content/themes/bienli/includes/partials/cakes.partial.php
@@ -4,13 +4,18 @@
 
     <?php while ( have_rows('cakes', 'option') ) : the_row();
         $name = get_sub_field('name','option');
-        $images = get_sub_field('images', 'option');
+        $image = get_sub_field('image', 'option');
         $link = get_sub_field('link', 'option');
 
+        if(!isset($image['url'])){
+//            $url = bloginfo('template_directory');
+            $image['url'] =  get_bloginfo('template_directory') . "/images/cake_default.png";
+        }
             ?>
            <div class="bienli-cakes-item-container col-lg-3 col-md-4 col-sm-4 col-xs-12">
                 <a href="<?php echo $link;?>">
                     <div class="bienli-cakes-item">
+                        <img src="<?php echo $image['url']; ?>"/>
                         <div class="bienli-cakes-item-title">
                             <?php echo $name; ?>
                         </div>
